e5993944d80f1c499cb566937341268eb8ad9d0f
Miko <mikogurrobat0@gmail.com>
2022-11-12 20:42:20 +0000

    projects page

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       },
       boxShadow: {
         '3xl': 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;',
-        'md': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;'
+        
       },
       margin: {
         '5': '75px',
