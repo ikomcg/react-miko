cd6be6557b61094a3874aa5f632b7a4cb0f0638e
Miko <mikogurrobat0@gmail.com>
2022-11-12 20:25:04 +0000

    projects page

--- src/Pages/Contacts/Contact.jsx
+++ src/Pages/Contacts/Contact.jsx
@@ -2,20 +2,20 @@ import React, { useEffect, useRef } from 'react'
 
 const Contact = () =>{
 
-    const link_loc = useRef(false);
+    const bol = useRef(false);
 
     useEffect(() =>{
        
         const loc = window.location.pathname
         switch(loc) {
             case '/home':
-                link_loc.current = false
+                bol.current = false
                 break;
             case '/project':
-                link_loc.current = true
+                bol.current = true
                 break;
             default:
-                link_loc.current = false
+                bol.current = false
         }
         
 
@@ -25,16 +25,41 @@ const Contact = () =>{
         <div className='contact-home flex flex-row justify-between px-8 mb-40'>
             <div className="left-form w-1/2 flex flex-col pr-10">
                 <div>
-                    <h3 className='font-black my-10'>
+                    {hr}
+                    <h3 className='font-black my-10 '>
                     let's talk about your thoughts
                     </h3>
-                    <p className='font-medium'>
+                    <p className='fonts-medium'>
                     This is my contacts and you can reach me out in many ways. You can  direct message me if you want, book a meeting so we can talk about your thoughts
                     </p>
                 </div>
+                {bol &&
+                    <div className='flex flex-col justify-center mt-10'>
+                        <ul className='flex flex-col '>
+                            <li className='shadow-2md'>
+                                <img src={gmail} alt="" />
+                                mikogurrobat0@gmail.com
+                            </li>
+                            <li className='shadow-2md'>
+                                <CgPhone className='text-2xl text-bgviol'/>
+                                09269244053
+                            </li>
+                        </ul>
+                        <div className='book-meeting mt-20'>
+                            <ul>
+                               <li className='hover:scale-105'>
+                                    <a href="https://calendly.com/mikogurrobat0" className=''>
+                                        <img src={schedule} alt="" />
+                                        Book a Meeting
+                                    </a>
+                                </li>    
+                            </ul>
+                        </div>
+                    </div>
+                }
             </div>
-            <div className="right-form w-1/2 rounded-xl p-10">
-          
+            <div className="right-form w-1/2 rounded-xl  p-10">
+               
             </div>
         </div>
     )
