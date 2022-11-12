0a8455ef1a5bf9b823ca8dccca329048638f8d11
Miko <mikogurrobat0@gmail.com>
2022-11-12 22:02:16 +0000

    projects page

--- src/Pages/About/components/Modal.jsx
+++ src/Pages/About/components/Modal.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 function Modal(props) {
-    const {certificate, view} = props
+    
 
     const CloseModal = () => {
-        view(i => false);
+        view.props(i => false);
     };
 
     return (
         <div className='show-image flex justify-center items-center fixed top-0 left-0 z-10 w-full h-full bg-slate-500 dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-40'
             onClick={CloseModal}>
                 <div className='flex justify-center items-center w-full mx-2 md:mx-0 md:w-6/12 md:h-6/12 relative'>
-                    <img src={certificate} className='h-480' alt="Certificate Image"/>
+                    <img src={certificate.props} className='h-480' alt="Certificate Image"/>
                 </div>
             </div>
     )
