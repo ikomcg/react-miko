9f2b691ae18f464200b743a80762b0f6e79eecba
Miko <mikogurrobat0@gmail.com>
2022-11-12 22:18:17 +0000

    projects page

--- src/Pages/About/components/Modal.jsx
+++ src/Pages/About/components/Modal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+
 function Modal(props) {
     
 
