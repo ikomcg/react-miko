94625c560ceb584485a6cca5b52ad125e4a7d371
Miko <mikogurrobat0@gmail.com>
2022-11-12 20:11:36 +0000

    projects page

--- src/Pages/Contacts/Contact.jsx
+++ src/Pages/Contacts/Contact.jsx
@@ -1,10 +1,24 @@
 
+
 const Contact = () =>{
 
-    
     return (
         <div className='contact-home flex flex-row justify-between px-8 mb-40'>
-           
+            <div className="left-form w-1/2 flex flex-col pr-10">
+                <div>
+                  
+                    <h3 className='font-black my-10'>
+                    let's talk about your thoughts
+                    </h3>
+                    <p className='font-medium'>
+                    This is my contacts and you can reach me out in many ways. You can  direct message me if you want, book a meeting so we can talk about your thoughts
+                    </p>
+                </div>
+                
+            </div>
+            <div className="right-form w-1/2 rounded-xl shadow-2md p-10">
+          
+            </div>
         </div>
     )
 }
