9eead62cdb4d833d027f3194104a0bc27a35dd9b
Miko <mikogurrobat0@gmail.com>
2022-11-12 22:04:25 +0000

    projects page

--- src/Pages/About/components/Modal.jsx
+++ src/Pages/About/components/Modal.jsx
@@ -4,14 +4,14 @@ function Modal(props) {
     
 
     const CloseModal = () => {
-        view.props(i => false);
+        props.view(i => false);
     };
 
     return (
         <div className='show-image flex justify-center items-center fixed top-0 left-0 z-10 w-full h-full bg-slate-500 dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-40'
             onClick={CloseModal}>
                 <div className='flex justify-center items-center w-full mx-2 md:mx-0 md:w-6/12 md:h-6/12 relative'>
-                    <img src={certificate.props} className='h-480' alt="Certificate Image"/>
+                    <img src={props.certificate} className='h-480' alt="Certificate Image"/>
                 </div>
             </div>
     )
