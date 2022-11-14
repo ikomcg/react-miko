0279ee026dfc242e302452560e09dc28b222c215
Miko <mikogurrobat0@gmail.com>
2022-11-14 09:46:36 +0000

    changes

--- src/Pages/NoPage.jsx
+++ src/Pages/NoPage.jsx
@@ -11,7 +11,7 @@ const NoPage = () => {
                         <TopRight />
                     <span>4</span>
                 </div>
-               <span className='text-2xl font-semibold'> OOPS, SORRY WE CAN'T FIND THAT PAGE!</span>
+               <span className='text-2xl font-semibold'> OOPS, SORRY CAN'T FIND THAT PAGE!</span>
                 <br />
                 <hr className='text-bgviol  bg-bgviol' />
                 <button className='bg-bgviol py-1 px-5 rounded shadow-2md my-4 text-white font-bold text-normal'>
